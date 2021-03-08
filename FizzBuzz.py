a4d1b35378fb0512ec0a8e0644bf2e30c09a96b4
Matthew Peterkort <peterkom@oregonstate.edu>
2021-03-07 16:32:16 -0800

    first commit setupt initial tests

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,17 +5,6 @@ def FizzBuzz():
         if(i%5==0 and i%3==0):
             print("FizzBuzz")
             j[i]="FizzBuzz"
-        elif (i%5==0):
-            print("Buzz")
-            j[i]="Buzz"
-            if(i==100):
-                return j
-        elif (i%3==0):
-            print("Fizz")
-            j[i]="Fizz"
-        else:
-            print(i)
-            j[i]=i
             
         i=i+1
   
